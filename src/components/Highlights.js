375b746e7111f81ac6eb991b581bc5454302d9ae
Justin Yum <Justin.Yum@am.sony.com>
2017-07-26 12:52:51 -0700

    added thumbnails to highlights

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react'
+import logo from '../logo.svg'
+
+const logoStyle = {
+  animation: 'App-logo-spin infinite 20s linear',
+  height: '80px'
+}
+
+//TODO Add "Thumbnails" component to "Highlights"
 
 class Highlights extends Component {
   render () {
     return (
       <div>
         <h1>This is a highlight.</h1>
+        <div class="row">
+          <div className="col-md-3">
+            <a href="#" class="thumbnail">
+              <img src={logo} alt="logo" style={logoStyle} />
+            </a>
+          </div>
+          <div className="col-md-3">
+            <a href="#" class="thumbnail">
+              <img src={logo} alt="logo" style={logoStyle} />
+            </a>
+          </div>
+          <div className="col-md-3">
+            <a href="#" class="thumbnail">
+              <img src={logo} alt="logo" style={logoStyle} />
+            </a>
+          </div>
+          <div className="col-md-3">
+            <a href="#" class="thumbnail">
+              <img src={logo} alt="logo" style={logoStyle} />
+            </a>
+          </div>
+        </div>
       </div>
     )
   }
