cac76c0a3a905aaef1f3d57139391a9219d9ac3a
Justin Yum <Justin.Yum@am.sony.com>
2017-07-26 15:58:16 -0700

    added Hanbit Church logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import logo from '../../logo.svg'
+import logo from '../../sdhanbit_logo_medium.png'
 import TopNavLink from './TopNavLink'
 
 const logoStyle = {
