2b1b64f7e08c1f7e906595d96d3873adcf8f110d
Justin Yum <Justin.Yum@am.sony.com>
2017-07-26 15:57:23 -0700

    added TODOs

--- src/components/Backgrounds.js
+++ src/components/Backgrounds.js
@@ -1,4 +1,26 @@
 import React, { Component } from 'react'
+import logo from '../logo.svg'
+
+const logoStyle = {
+  animation: 'App-logo-spin infinite 20s linear',
+  height: '80px'
+}
+
+// QUESTION How do I add Bootstrap-4 functionality?
+// IDEA Add this code block to add carousel for background
+// <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+//   <div className="carousel-inner" role="listbox">
+//     <div className="carousel-item active">
+//       <img src={logo} alt="logo" style={logoStyle} />
+//     </div>
+//     <div className="carousel-item">
+//       <img src={logo} alt="logo" style={logoStyle} />
+//     </div>
+//     <div className="carousel-item">
+//       <img src={logo} alt="logo" style={logoStyle} />
+//     </div>
+//   </div>
+// </div>
 
 class Backgrounds extends Component {
   render () {
