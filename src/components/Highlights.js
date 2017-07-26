d89b4bf684f1fcac2948a95cfa3e888d4d5a62b6
Justin Yum <Justin.Yum@am.sony.com>
2017-07-26 15:58:40 -0700

    added example text

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -6,31 +6,29 @@ const logoStyle = {
   height: '80px'
 }
 
-//TODO Add "Thumbnails" component to "Highlights"
-
 class Highlights extends Component {
   render () {
     return (
       <div>
         <h1>This is a highlight.</h1>
-        <div class="row">
+        <div className="row">
           <div className="col-md-3">
-            <a href="#" class="thumbnail">
+            <a href="#" className="thumbnail">
               <img src={logo} alt="logo" style={logoStyle} />
             </a>
           </div>
           <div className="col-md-3">
-            <a href="#" class="thumbnail">
+            <a href="#" className="thumbnail">
               <img src={logo} alt="logo" style={logoStyle} />
             </a>
           </div>
           <div className="col-md-3">
-            <a href="#" class="thumbnail">
+            <a href="#" className="thumbnail">
               <img src={logo} alt="logo" style={logoStyle} />
             </a>
           </div>
           <div className="col-md-3">
-            <a href="#" class="thumbnail">
+            <a href="#" className="thumbnail">
               <img src={logo} alt="logo" style={logoStyle} />
             </a>
           </div>
--- src/components/direction/index.js
+++ src/components/direction/index.js
@@ -8,6 +8,7 @@ class Direction extends Component {
       <div>
         <Header />
         <h1>This is a direction page.</h1>
+        <h3>Address: 4717 Cardin St, San Diego, CA 92111</h3>
         <Footer />
       </div>
     )
