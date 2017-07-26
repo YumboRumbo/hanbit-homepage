827ef5fe3c2b6787ce6c9b28c507bc0fed14d303
Justin Yum <Justin.Yum@am.sony.com>
2017-07-26 12:05:45 -0700

    fixed html tags

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import React, {
-  Component
-} from 'react'
-import {
-  Router,
-  Route,
-  hashHistory
-} from 'react-router'
+import React, { Component } from 'react'
+import { Router, Route, hashHistory } from 'react-router'
 import Home from './components/home'
 import Worship from './components/worship'
 import Community from './components/community'
@@ -15,48 +9,21 @@ import Direction from './components/direction'
 import 'bootstrap/less/bootstrap.less'
 
 class App extends Component {
-  render() {
-    return ( <
-      div className = "row" >
-      <
-      div className = "col-md-2" / >
-      <
-      div className = "col-md-8" >
-      <
-      Router history = {
-        hashHistory
-      } >
-      <
-      Route path = "/"
-      component = {
-        Home
-      }
-      /> <
-      Route path = "/worship"
-      component = {
-        Worship
-      }
-      /> <
-      Route path = "/community"
-      component = {
-        Community
-      }
-      /> <
-      Route path = "/mission"
-      component = {
-        Mission
-      }
-      /> <
-      Route path = "/direction"
-      component = {
-        Direction
-      }
-      /> <
-      /Router> <
-      /div> <
-      div className = "col-md-2" / >
-      <
-      /div>
+  render () {
+    return (
+      <div className="row">
+        <div className="col-md-2"/>
+        <div className="col-md-8">
+          <Router history={hashHistory}>
+            <Route path="/" component={Home}/>
+            <Route path="/worship" component={Worship}/>
+            <Route path="/community" component={Community}/>
+            <Route path="/mission" component={Mission}/>
+            <Route path="/direction" component={Direction}/>
+          </Router>
+        </div>
+        <div className="col-md-2" />
+      </div>
     )
   }
 }
